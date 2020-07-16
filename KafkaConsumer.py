87af98b5b7c2c224877edc18fe0528a8adb88824
Harshdev <hrandhawa1993@gmail.com>
2020-07-16 02:07:15 +0100

    modified producer/consumer for stock data separation

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -3,31 +3,112 @@ import boto3
 import json
 
 
-c = Consumer({
-    'bootstrap.servers': 'xxxxx:9092',
-    'group.id': 'mygroup2',
+aapl_consumer = Consumer({
+    'bootstrap.servers': 'xxxxxx:9092',
+    'group.id': 'stock-consumer-group',
     'auto.offset.reset': 'latest'
 })
 
-c.subscribe(['testtopic'])
+aapl_consumer.subscribe(['aapl-topic'])
+
+tsla_consumer = Consumer({
+    'bootstrap.servers': 'xxxxxx:9092',
+    'group.id': 'stock-consumer-group',
+    'auto.offset.reset': 'latest'
+})
+
+tsla_consumer.subscribe(['tsla-topic'])
+
+googl_consumer = Consumer({
+    'bootstrap.servers': 'xxxxxx:9092',
+    'group.id': 'stock-consumer-group',
+    'auto.offset.reset': 'latest'
+})
+
+googl_consumer.subscribe(['googl-topic'])
+
+fb_consumer = Consumer({
+    'bootstrap.servers': 'xxxxxx:9092',
+    'group.id': 'stock-consumer-group',
+    'auto.offset.reset': 'latest'
+})
+
+fb_consumer.subscribe(['fb-topic'])
+
+amzn_consumer = Consumer({
+    'bootstrap.servers': 'xxxxxx:9092',
+    'group.id': 'stock-consumer-group',
+    'auto.offset.reset': 'latest'
+})
+
+amzn_consumer.subscribe(['amzn-topic'])
+
+
 
 while True:
-    msg = c.poll(1.0)
+    aapl_msg = aapl_consumer.poll(1.0)
+    tsla_msg = tsla_consumer.poll(1.0)
+    googl_msg = googl_consumer.poll(1.0)
+    fb_msg = fb_consumer.poll(1.0)
+    amzn_msg = amzn_consumer.poll(1.0)
 
-    if msg is None:
-        continue
-    if msg.error():
-        print("Consumer error: {}".format(msg.error()))
-        continue
+    # if aapl_msg is None:
+    #     continue
+    # if tsla_msg is None:
+    #     continue
+    # if googl_msg is None:
+    #     continue
+    # if fb_msg is None:
+    #     continue
+    # if amzn_msg is None:
+    #     continue
 
-    # print('Received message: {}'.format(msg.value().decode('utf-8')))
-    x = msg.value().decode('utf-8')
+    # if aapl_msg.error():
+    #     print("Consumer error: {}".format(aapl_msg.error()))
+    #     continue
+    # if tsla_msg.error():
+    #     print("Consumer error: {}".format(tsla_msg.error()))
+    #     continue
+    # if googl_msg.error():
+    #     print("Consumer error: {}".format(googl_msg.error()))
+    #     continue
+    # if fb_msg.error():
+    #     print("Consumer error: {}".format(fb_msg.error()))
+    #     continue
+    # if aapl_msg.error():
+    #     print("Consumer error: {}".format(aapl_msg.error()))
+    #     continue
 
-    s3_client = boto3.client('s3')
+    if aapl_msg is not None:
+        print('Received message from AAPL Topic: {}'.format(aapl_msg.value().decode('utf-8')))
+    if tsla_msg is not None:
+        print('Received message from TSLA Topic: {}'.format(tsla_msg.value().decode('utf-8')))
+    if googl_msg is not None:
+        print('Received message from GOOGL Topic: {}'.format(googl_msg.value().decode('utf-8')))
+    if fb_msg is not None:
+        print('Received message from FB Topic: {}'.format(fb_msg.value().decode('utf-8')))
+    if amzn_msg is not None:
+        print('Received message from AMZN Topic: {}'.format(amzn_msg.value().decode('utf-8')))
 
-    x = bytes(json.dumps(x).encode('UTF-8'))
+    # print('Received message from TSLA Topic: {}'.format(tsla_msg.value().decode('utf-8')))
+    # print('Received message from GOOGL Topic: {}'.format(googl_msg.value().decode('utf-8')))
+    # print('Received message from FB Topic: {}'.format(fb_msg.value().decode('utf-8')))
+    # print('Received message from AMZN Topic: {}'.format(amzn_msg.value().decode('utf-8')))
 
-    s3_client.put_object(Body=x, Bucket='xxx-xxxx', Key='stock-data')
+    # x = amzn_msg.value().decode('utf-8')
+    #
+    # s3_client = boto3.client('s3')
+    #
+    # x = bytes(json.dumps(x).encode('UTF-8'))
+    #
+    # s3_client.put_object(Body=x, Bucket='zxcvcbdbx', Key='stock-data')
     # print(x)
 
-c.close()
+# c.close()
+# d.close()
+
+aapl_consumer.close()
+tsla_consumer.close()
+googl_consumer.close()
+fb_consumer.close()
+amzn_consumer.close()
